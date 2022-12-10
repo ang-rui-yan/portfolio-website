585a8dcb4729a8c8d3c1d5d23af32c4c59063025
Rui Yan <angruiyan@gmail.com>
2022-12-10 21:24:22 +0800

    Fixed image and added education content

--- src/Components/AboutMeSection.js
+++ src/Components/AboutMeSection.js
@@ -1,6 +1,7 @@
 import { Button, styled, Typography, useMediaQuery } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 import React from 'react';
+import standingImage from '../assets/images/standing.png';
 
 function AboutMeSection() {
 	const CustomBodyText = styled('div')(({ theme }) => ({
@@ -30,7 +31,12 @@ function AboutMeSection() {
 					color: 'white',
 				}}
 			>
-				<Typography>About Me</Typography>
+				<Typography
+					variant="h4"
+					component="div"
+				>
+					About Me
+				</Typography>
 
 				<div
 					style={{
@@ -42,28 +48,52 @@ function AboutMeSection() {
 						style={{ marginTop: 5, marginBottom: 10 }}
 						width={breakpoints.isXs || breakpoints.isSm ? '55%' : '20%'}
 						height="70%"
-						viewBox="0 0 161 161"
+						viewBox="0 0 185 192"
 						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
 					>
 						<rect
-							x="-0.308594"
-							y="12.5919"
+							x="10.6914"
+							y="42.5919"
 							width="149.194"
 							height="149.194"
 							rx="8"
-							transform="rotate(-5 -0.308594 12.5919)"
+							transform="rotate(-5 10.6914 42.5919)"
 							fill="#A0B7E2"
 						/>
 						<rect
-							x="5.9082"
-							y="5.80646"
+							x="16.9082"
+							y="35.8065"
 							width="149.194"
 							height="149.194"
 							rx="8"
 							fill="#DBE2EF"
 						/>
+						<rect
+							width="185"
+							height="185"
+							fill="url(#pattern0)"
+						/>
+						<defs>
+							<pattern
+								id="pattern0"
+								patternContentUnits="objectBoundingBox"
+								width="1"
+								height="1"
+							>
+								<use
+									href="#selfImage"
+									transform="translate(0 -0.166282) scale(0.00230947)"
+								/>
+							</pattern>
+							<image
+								id="selfImage"
+								width="433"
+								height="870"
+								href={standingImage}
+							/>
+						</defs>
 					</svg>
+
 					<CustomBodyText>
 						I am currently pursuing a degree in Computer Science in Singapore Management
 						University (SMU).
--- src/Components/EducationSection.js
+++ src/Components/EducationSection.js
@@ -1,16 +1,64 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function TimelineComponent({ timeline, university, content }) {
+	return (
+		<div>
+			<h2>{timeline}</h2>
+			<h3>{university}</h3>
+			<ul>
+				{content.map((detail) => (
+					<li>{detail}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
 
 function EducationSection() {
+	const data = [
+		{
+			timeline: 'Aug 2022 - Jul 2026',
+			university: 'Singapore Management University',
+			content: [
+				'Pursuing Bachelor of Science - Computer Science',
+				'Expected year of graduation in 2026',
+				'Looking to pick a track in Artifical Intelligence or Cyberphysical system',
+			],
+		},
+		{
+			timeline: 'Apr 2017 - Mar 2020',
+			university: 'Temasek Polytechnic',
+			content: [
+				'Graduated with Diploma with Merit in Financial Business Informatics',
+				'Achieved 2nd in cohort',
+				'Graduated with Diploma Plus in Entrepreneurship and Innovation',
+			],
+		},
+	];
 	return (
 		<>
 			<div
 				style={{
-					padding: "20px",
-					paddingTop: "60px",
+					padding: '20px',
+					paddingTop: '60px',
 				}}
 			>
-				<Typography>Education</Typography>
+				<Typography
+					variant="h4"
+					component="div"
+				>
+					Education
+				</Typography>
+				<div style={{ margin: 'auto', width: '50%' }}>
+					{data.map((point) => (
+						<TimelineComponent
+							timeline={point.timeline}
+							university={point.university}
+							content={point.content}
+						/>
+					))}
+				</div>
 			</div>
 		</>
 	);
