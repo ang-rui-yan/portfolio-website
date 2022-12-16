4880f1a0fdfa36b8c1592785cf5edf3f1e07c192
Rui Yan <angruiyan@gmail.com>
2022-12-16 11:49:01 +0800

    gitignore is now working

--- src/data/navbar.js
+++
@@ -1,7 +0,0 @@
-export let pages = [
-	{ label: 'About me', element: 'aboutMe' },
-	{ label: 'Education', element: 'education' },
-	{ label: 'Skills', element: 'skills' },
-	{ label: 'Projects', element: 'projects' },
-	{ label: 'Contact me', element: 'contactMe' },
-];
--- src/data/urls.js
+++
@@ -1,4 +0,0 @@
-export let externalUrls = {
-	linkedin: 'https://www.linkedin.com/in/angruiyan/',
-	github: 'https://github.com/ang-rui-yan',
-};
